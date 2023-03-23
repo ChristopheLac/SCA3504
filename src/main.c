405d05813ea290dd0b8f5f778d036aaa8c4e79b3
BeStiled <GitHubBe@stiled.fr>
2023-03-23 07:20:57 +0100

    code en morceau: ledPwm

--- src/main.c
+++ src/main.c
@@ -165,91 +165,6 @@ void print_uart(char *buf)
 }
 #endif
 
-//Code pour pwd
-
-#define LED_PWM_NODE_ID	 DT_COMPAT_GET_ANY_STATUS_OKAY(pwm_leds)
-
-const char *led_label[] = {
-	DT_FOREACH_CHILD_SEP_VARGS(LED_PWM_NODE_ID, DT_PROP_OR, (,), label, NULL)
-};
-
-const int num_leds = ARRAY_SIZE(led_label);
-
-#define MAX_BRIGHTNESS	100
-
-#define FADE_DELAY_MS	10
-#define FADE_DELAY	K_MSEC(FADE_DELAY_MS)
-
-/**
- * @brief Run tests on a single LED using the LED API syscalls.
- *
- * @param led_pwm LED PWM device.
- * @param led Number of the LED to test.
- */
-static void run_led_test(const struct device *led_pwm, uint8_t led)
-{
-	int err;
-	uint16_t level;
-
-	LOG_INF("Testing LED %d - %s", led, led_label[led] ? : "no label");
-
-	/* Turn LED on. */
-	err = led_on(led_pwm, led);
-	if (err < 0) {
-		LOG_ERR("err=%d", err);
-		return;
-	}
-	LOG_INF("  Turned on");
-	k_sleep(K_MSEC(1000));
-
-	/* Turn LED off. */
-	err = led_off(led_pwm, led);
-	if (err < 0) {
-		LOG_ERR("err=%d", err);
-		return;
-	}
-	LOG_INF("  Turned off");
-	k_sleep(K_MSEC(1000));
-
-	/* Increase LED brightness gradually up to the maximum level. */
-	LOG_INF("  Increasing brightness gradually");
-	for (level = 0; level <= MAX_BRIGHTNESS; level++) {
-		err = led_set_brightness(led_pwm, led, level);
-		if (err < 0) {
-			LOG_ERR("err=%d brightness=%d\n", err, level);
-			return;
-		}
-		k_sleep(FADE_DELAY);
-	}
-	k_sleep(K_MSEC(1000));
-
-	/* Set LED blinking (on: 0.1 sec, off: 0.1 sec) */
-	err = led_blink(led_pwm, led, 100, 100);
-	if (err < 0) {
-		LOG_ERR("err=%d", err);
-		return;
-	}
-	LOG_INF("  Blinking on: 0.1 sec, off: 0.1 sec");
-	k_sleep(K_MSEC(5000));
-
-	/* Enable LED blinking (on: 1 sec, off: 1 sec) */
-	/*err = led_blink(led_pwm, led, 65, 131);
-	if (err < 0) {
-		LOG_ERR("err=%d", err);
-		LOG_INF("  Cycle period not supported - on: 1 sec, off: 1 sec");
-	} else {
-		LOG_INF("  Blinking on: 1 sec, off: 1 sec");
-	}
-	k_sleep(K_MSEC(5000));*/
-
-	/* Turn LED off. */
-	err = led_off(led_pwm, led);
-	if (err < 0) {
-		LOG_ERR("err=%d", err);
-		return;
-	}
-	LOG_INF("  Turned off, loop end");
-}
 
 //code pour le Bluetooth
 #if (BLUETOOTH > 0)
@@ -865,20 +780,6 @@ void main(void)
 #endif // BLUETOOTH > 0
 	//pour PWD
 
-	const struct device *led_pwm;
-//	uint8_t led;
-
-	led_pwm = DEVICE_DT_GET(LED_PWM_NODE_ID);
-
-	if (!device_is_ready(led_pwm)) {
-		LOG_ERR("Device %s is not ready", led_pwm->name);
-		return;
-	}
-
-	if (!num_leds) {
-		LOG_ERR("No LEDs found for %s", led_pwm->name);
-		return;
-	}
 
 	if (!device_is_ready(led.port)) {
 		return;
@@ -1041,9 +942,9 @@ int ret;
 			//k_msleep(sleep);
 			comande = 0;
 			//pour PWM
-			for (int num_led = 0; num_led < num_leds; num_led++) {
-			run_led_test(led_pwm, num_led);
-			}
+//			for (int num_led = 0; num_led < num_leds; num_led++) {
+//			run_led_test(led_pwm, num_led);
+//			}
 			//the end PWM.
 
 		}
